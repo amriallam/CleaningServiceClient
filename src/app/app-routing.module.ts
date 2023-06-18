fc03e5f6aa84f3975cb289e7743ec328c7c4028a
AmrAllam <98554346+amriallam@users.noreply.github.com>
2023-06-19 02:50:02 +0300

    routing update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,8 +17,7 @@ const routes: Routes = [
   { path: '', pathMatch: "full", component: HomeComponent},
   // { path: "service", loadChildren: () => import("./Components/service/service.module").then(m => m.ServiceModule) },
   // { path: '', pathMatch: "full", redirectTo: 'home' },
-  {path:'home',component: HomeComponent},
-  { path: "", loadChildren: () => import("./components/service/service.module").then(m => m.ServiceModule) },
+  { path: "service/:servicename", loadChildren: () => import("./components/service/service.module").then(m => m.ServiceModule) },
   // { path: "resource", loadChildren: () => import("./Components/resource/resource.module").then(m => m.ResourceModule) },
   // { path: "booking", loadChildren: () => import("./Components/booking/booking.module").then(m => m.BookingModule) },
 
