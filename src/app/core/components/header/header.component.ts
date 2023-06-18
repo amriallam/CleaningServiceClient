66da262ce68a6012eb76631245b2dd20551c8746
AmrAllam <98554346+amriallam@users.noreply.github.com>
2023-06-19 01:10:06 +0300

    header comoponent

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { LoginComponent } from 'src/app/shared/components/login/login.component';
-// import { ResourceTypeService } from '../../services/resource-type.service';
-// import { ResourceType } from '../../Models/ResourceType';
 
 @Component({
   selector: 'app-header',
@@ -12,31 +8,11 @@ import { LoginComponent } from 'src/app/shared/components/login/login.component'
 })
 
 export class HeaderComponent {
-  languages = [
-    { name: "Arabic", value: 'ar' },
-    { name: "English", value: 'en' }
-  ]
-  selectedLanguage: string = "en";
-
-  updateSelectedLanguage(value: string) {
-    this.selectedLanguage = value;
-  }
-
-  checkIfLoggedIn() {
-    return localStorage.getItem('userBookingAppToken');
-  }
-  logout() {
-    localStorage.removeItem('userBookingAppToken');
-  }
-
-  // resourceTypes: ResourceType[] = [];
-
-  constructor() { }
-
-
-  // ngOnInit(): void {
-  //   this.resourceTypeService.GetAll().subscribe(data =>
-  //     this.resourceTypes = data.data
-  //   )
+  // checkIfLoggedIn() {
+  //   return localStorage.getItem('userBookingAppToken');
   // }
+  // logout() {
+  //   localStorage.removeItem('userBookingAppToken');
+  // }
+  constructor() { }
 }
