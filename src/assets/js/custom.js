fd5a8f35fe12f9bdd938140d958ec7d36a5aaec1
AmrAllam <98554346+amriallam@users.noreply.github.com>
2023-06-19 03:20:02 +0300

    home page updated

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -7,9 +7,9 @@
 
     // BACKSTRETCH SLIDESHOW
     $('.hero-section').backstretch([
-      "../images/slideshow/afro-woman-cleaning-window-with-rag-home.jpg",
-      "../images/slideshow/afro-woman-holding-bucket-with-cleaning-items.jpg",
-      "../images/slideshow/unrecognizable-cleaner-walking-into-hotel-room-with-tools-detergents.jpg"
+      "../../assets/images/slideshow/afro-woman-cleaning-window-with-rag-home.jpg",
+      "../../assets/images/slideshow/afro-woman-holding-bucket-with-cleaning-items.jpg",
+      "../../assets/images/slideshow/unrecognizable-cleaner-walking-into-hotel-room-with-tools-detergents.jpg"
     ],  {duration: 2000, fade: 750});
 
   })(window.jQuery);
