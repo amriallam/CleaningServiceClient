aeedf5f568d1887113ee7e4e68a4b5214b1f44eb
AmrAllam <98554346+amriallam@users.noreply.github.com>
2023-06-20 01:30:18 +0300

    deleted useless services , models

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './core/components/home/home.component';
-// import { ForgetPasswordComponent } from './shared/components/forget-password/forget-password.component';
-// import { LoginComponent } from './shared/components/login/login.component';
-// import { RegistrationComponent } from './shared/components/registration/registration.component';
 import { AboutUsComponent } from './shared/components/about-us/about-us.component';
 import { ContactUsComponent } from './shared/components/contact-us/contact-us.component';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
-import { AccountModule } from './core/account/account.module';
-// Login and register components from account module
 import { LoginComponent } from './core/account/login/login.component';
 import { RegistrationComponent } from './core/account/registration/registration.component';
 import { ForgetPasswordComponent } from './core/account/forget-password/forget-password.component';
@@ -21,16 +16,7 @@ const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
   {
-    path: 'service/:servicename',
-    loadChildren: () =>
-      import('./components/service/service.module').then(
-        (m) => m.ServiceModule
-      ),
-  },
-  {
-    path: 'account',
-    loadChildren: () =>
-      import('../app/core/account/account.module').then((m) => m.AccountModule),
+    path: 'service/:servicename', loadChildren: () => import('./components/service/service.module').then( (m) => m.ServiceModule ),
   },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegistrationComponent },
@@ -51,4 +37,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
