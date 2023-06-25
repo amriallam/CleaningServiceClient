570bcbc61987e1df3216178371db2007a6e4326e
Aya Ulshenawy <ulshenawya@gmail.com>
2023-06-26 00:01:24 +0300

    updates

--- src/app/core/ViewModels/booking-details-vm.ts
+++ src/app/core/ViewModels/booking-details-vm.ts
@@ -4,7 +4,6 @@ export class BookingDetailsVM {
     public date?: string,
     public from?: string,
     public to?: string,
-    public location?: string,
     public totalCost?: number,
     public regionId?:number,
     public selectedResIds?: number[],
