a9685f12f3ee65ccfe046934409f0148fd9e5125
Amr Allam <amr.i.allam@hotmail.com>
2023-06-30 08:22:31 +0300

    fixed routing

--- src/app/core/account/login/login.component.ts
+++ src/app/core/account/login/login.component.ts
@@ -5,6 +5,8 @@ import { ToastrService } from 'ngx-toastr';
 import { LoginService } from 'src/app/core/services/login.service';
 import jwt_decode from 'jwt-decode';
 import { BookingService } from '../../services/booking.service';
+import { ServiceService } from '../../services/service.service';
+import { UserService } from '../../services/User.service';
 
 @Component({
   selector: 'app-login',
@@ -22,15 +24,10 @@ export class LoginComponent {
     private router: Router,
     private toastr: ToastrService,
     private loginService: LoginService,
-    private bookinService : BookingService 
+    private bookinService: BookingService,
+    private userService:UserService
   ) {
     this.loginFormGroup = new FormGroup({
-      // ssn: new FormControl('', [
-      //   Validators.required,
-      //   Validators.minLength(14),
-      //   Validators.maxLength(14),
-      //   Validators.pattern('^[0-9]*$'),
-      // ]),
       email: new FormControl('', {
         updateOn: 'submit',
         validators: [
@@ -71,10 +68,7 @@ export class LoginComponent {
 
   onSubmit() {
     if (this.loginFormGroup.invalid) {
-      // alert('Please fill all the required fields');ValidationFE()
       this.ValidationFE();
-
-      // return this.userRegisterForm.controls['email'].errors
     } else {
       this.emailFEValidation = false;
       this.passwordFEValidation = false;
@@ -84,87 +78,19 @@ export class LoginComponent {
           this.loginFormGroup.value.password
         )
         .subscribe((res) => {
-         
-
-          console.log(jwt_decode(res.data.token))
           if (res.data.token) {
-            console.log('resssss'); 
-            console.log(res.data.token);
             this.userBookingAppId = this.jwtDecode(res).Id;
             localStorage.setItem('userBookingAppId', this.userBookingAppId);
             localStorage.setItem('userBookingAppToken', res.data.token);
-            // show toast from ngx-toastr
+            this.userService.upDateUser(this.userBookingAppId)
             this.toastr.success('Login Successful', 'Welcome');
             setTimeout(() => {
-              if(this.bookinService.bookingDetails != null){
-                this.router.navigate(['/booking/bookingList'])
-              }
-              else{
-                 this.router.navigate(['/']);
-              }
+              if (this.bookinService.bookingDetails.serviceId)
+               this.router.navigate(['/booking/bookingList']);
+               else this.router.navigate(['/']);
             }, 1000);
-          } else {
-            console.log(res);
-            // return res.json().then((data: any) => {
-            //   console.log(data);
-            //   // missing fields
-            //   if (data.errors) {
-            //     let errors = data.errors;
-            //     this.missingFeildsErrosObject = errors;
-            //   }
-            //   // field not valid
-            //   else if (data[0].description) {
-            //     // show toast from ngx-toastr
-            //     this.toastr.error(data[0].description, 'Login Failed');
-            //   }
-            // });
-          }
-          // else {
-          //   alert('else block')
-          //   return;
-          // }
+          } else console.log(res);
         });
     }
   }
 }
-// fetch('https://localhost:7158/api/Account/login', {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(this.loginFormGroup.value),
-//       })
-//         .then((res) => {
-//           if (res.status === 200) {
-//             res.json().then((data) => {
-
-//               localStorage.setItem('userBookingAppToken', data.token);
-//               // show toast from ngx-toastr
-//               this.toastr.success('Login Successful', 'Welcome');
-//              setTimeout(() => {
-//               this.router.navigate(['/']);
-//              }, 3000);
-//             });
-
-//             return;
-//           } else if (res.status === 400) {
-//             return res.json().then((data) => {
-//               console.log(data);
-//               // missing fields
-//               if (data.errors) {
-//                 let errors = data.errors;
-//                 this.missingFeildsErrosObject = errors;
-//               }
-//               // field not valid
-//               else if (data[0].description) {
-//                 // show toast from ngx-toastr
-//                 this.toastr.error(data[0].description, 'Login Failed');
-//               }
-//             });
-//           } else {
-//             return;
-//           }
-//         })
-//         .catch((err) => {
-//           this.toastr.error('please enter valid email or password');
-//         });
--- src/app/core/services/User.service.ts
+++ src/app/core/services/User.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable ,of } from 'rxjs';
+import { Observable ,Subject,of } from 'rxjs';
 import { apiUrl } from 'src/environment'
 import {User} from '../Models/UserModel';
 import { ResponseModel } from '../Models/ResponseModel';
@@ -11,7 +11,10 @@ import { ResponseModel } from '../Models/ResponseModel';
 })
 export class UserService{
     constructor(private http: HttpClient) { }
-
+    user$=new Subject<string>();
+    upDateUser(userId:string){
+      this.user$.next(userId);
+    }
     GetUserById(id:string): Observable<ResponseModel<User>> {
       return this.http.get<ResponseModel<User>>(apiUrl + "Account/GetUser/"+id)
     }
